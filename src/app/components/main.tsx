68cb6ac1711935058dc009f2dd3823c4356f16a4
Felipe Fransa <antoniofelipesilva@live.com>
2023-10-02 18:21:14 -0300

    main imgs add

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -1,8 +1,23 @@
 export const Main = () => {
     return (
         <div className="w-full h-full flex flex-col sm:flex-row justify-around items-center mb-2 mt-2">
-            <div className="w-full h-96 flex justify-center items-center bg-blue-700 rounded-md">
-                <h1>Imagen</h1>
+            <div className="w-full h-96 flex justify-around items-center bg-blue-700 rounded-md">
+
+                <div className="w-48 h-48 flex justify-center items-center bg-black rounded-md">
+                    <h1>Imagen...</h1>
+                </div>
+                <div className="w-48 h-48 flex justify-center items-center bg-black rounded-md">
+                    <h1>Imagen...</h1>
+                </div>
+                <div className="w-48 h-48 flex justify-center items-center bg-black rounded-md">
+                    <h1>Imagen...</h1>
+                </div>
+                <div className="w-48 h-48 flex justify-center items-center bg-black rounded-md">
+                    <h1>Imagen...</h1>
+                </div>
+                <div className="w-48 h-48 flex justify-center items-center bg-black rounded-md">
+                    <h1>Imagen...</h1>
+                </div>
             </div>
         </div>
     )
