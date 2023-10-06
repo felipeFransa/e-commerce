c6a9ab20cef02b8a543724849c3d91f2d7c32938
Felipe Fransa <antoniofelipesilva@live.com>
2023-10-06 18:25:51 -0300

    add footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return (
-        <div>
-            <div>
+        <div className="w-full h-full justify-between items-center">
+            <div className="w-full h-full flex flex-col sm:flex-row">
                 <div></div>
             </div>
         </div>
