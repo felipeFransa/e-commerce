6a2d82fd072e4051ccce9c74e36ba3b7ef1b0bfb
Felipe Fransa <antoniofelipesilva@live.com>
2023-10-11 15:54:26 -0300

    footer & partners

--- src/app/components/partners.tsx
+++ src/app/components/partners.tsx
@@ -6,10 +6,10 @@ export const Partners = () => {
     return (
         <div className="w-full h-full flex justify-between items-center">
             <div className="w-full h-full mt-2 flex flex-col sm:flex-row justify-between items-center">
-                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-96 h-64" src={Microsoft.src} alt="Logo microsoft" /></div>
-                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-96 h-64" src={Amazon.src} alt="Logo amazon" /></div>
-                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-96 h-64" src={Microsoft.src} alt="Logo microsoft" /></div>
-                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-96 h-64" src={Amazon.src} alt="Logo amazon" /></div>
+                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-80 h-64 rounded-md" src={Microsoft.src} alt="Logo microsoft" /></div>
+                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-80 h-64 rounded-md" src={Amazon.src} alt="Logo amazon" /></div>
+                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-80 h-64 rounded-md" src={Microsoft.src} alt="Logo microsoft" /></div>
+                <div className="w-full h-full flex justify-center items-center rounded-md"><img className="w-80 h-64 rounded-md" src={Amazon.src} alt="Logo amazon" /></div>
             </div>
         </div>
     )
