fa614e3eda137a0e4bfd5791a638f834c43509e5
Felipe Fransa <antoniofelipesilva@live.com>
2023-10-11 16:46:10 -0300

    new footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
     return (
         <div className="w-full h-full justify-between items-center">
-            <div className="w-full h-80 flex flex-col sm:flex-row justify-center items-center">
-                <div className="text-sm text-center">Copyright © 2023 por 333 Developer. Todos os direitos reservados. Nenhuma parte deste conteúdo pode ser reproduzida, distribuída ou transmitida de qualquer forma ou por qualquer meio, eletrônico ou mecânico, incluindo fotocópia, gravação ou qualquer outro sistema de armazenamento e recuperação de informações, sem permissão por escrito do detentor dos direitos autorais. Violações serão processadas de acordo com as leis de direitos autorais aplicáveis.</div>
+            <div className="w-full h-60 flex flex-col sm:flex-row justify-center items-end mb-4">
+                <div className="text-xs text-center">Copyright © 2023 por 333 Developer. Todos os direitos reservados. Nenhuma parte deste conteúdo pode ser reproduzida, distribuída ou transmitida de qualquer forma ou por qualquer meio, eletrônico ou mecânico, incluindo fotocópia, gravação ou qualquer outro sistema de armazenamento e recuperação de informações, sem permissão por escrito do detentor dos direitos autorais. Violações serão processadas de acordo com as leis de direitos autorais aplicáveis.</div>
             </div>
         </div>
     )
