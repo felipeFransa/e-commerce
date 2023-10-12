9b2ba6c210ae35c56aa362bd94a10417711c87f1
Felipe Fransa <antoniofelipesilva@live.com>
2023-10-12 18:42:15 -0300

    db fake

---
+++ src/app/db.tsx
@@ -0,0 +1,75 @@
+const produtosEletronicos = [
+    {
+      ID: 1,
+      Nome: "Notebook Dell XPS 15",
+      Descrição: "Notebook de alto desempenho com tela de 15 polegadas, processador Intel Core i7, 16GB de RAM e SSD de 512GB",
+      Preço: 1999.99,
+      Estoque: 50
+    },
+    {
+      ID: 2,
+      Nome: "Smartphone Samsung Galaxy S20",
+      Descrição: "Smartphone premium com tela AMOLED de 6,2 polegadas, câmera de 64MP, 128GB de armazenamento e 8GB de RAM",
+      Preço: 899.99,
+      Estoque: 100
+    },
+    {
+      ID: 3,
+      Nome: "Smart TV LG OLED 4K 55 polegadas",
+      Descrição: "Smart TV OLED com resolução 4K, suporte HDR, sistema operacional webOS e 4 portas HDMI",
+      Preço: 1499.99,
+      Estoque: 30
+    },
+    {
+      ID: 4,
+      Nome: "Notebook HP Pavilion 14",
+      Descrição: "Notebook de médio porte com tela de 14 polegadas, processador Intel Core i5, 8GB de RAM e SSD de 256GB",
+      Preço: 799.99,
+      Estoque: 75
+    },
+    {
+      ID: 5,
+      Nome: "Smartphone Apple iPhone 12",
+      Descrição: "iPhone com tela Retina de 6,1 polegadas, câmera dupla de 12MP, 64GB de armazenamento e 4GB de RAM",
+      Preço: 1099.99,
+      Estoque: 50
+    },
+    {
+      ID: 6,
+      Nome: "Smart TV Sony 4K 65 polegadas",
+      Descrição: "Smart TV LED com resolução 4K, Android TV, suporte HDR, e 3 portas HDMI",
+      Preço: 1299.99,
+      Estoque: 40
+    },
+    {
+      ID: 7,
+      Nome: "Notebook Asus ZenBook 13",
+      Descrição: "Notebook ultrafino com tela de 13 polegadas, processador Intel Core i7, 16GB de RAM e SSD de 512GB",
+      Preço: 1199.99,
+      Estoque: 25
+    },
+    {
+      ID: 8,
+      Nome: "Smartphone Google Pixel 6",
+      Descrição: "Smartphone Android com tela OLED de 6,4 polegadas, câmera principal de 50MP, 128GB de armazenamento e 8GB de RAM",
+      Preço: 849.99,
+      Estoque: 60
+    },
+    {
+      ID: 9,
+      Nome: "Smart TV Samsung QLED 75 polegadas",
+      Descrição: "Smart TV QLED com resolução 4K, Tizen OS, suporte HDR10+, e 4 portas HDMI",
+      Preço: 1999.99,
+      Estoque: 20
+    },
+    {
+      ID: 10,
+      Nome: "Notebook Lenovo ThinkPad X1 Carbon",
+      Descrição: "Notebook empresarial com tela de 14 polegadas, processador Intel Core i7, 16GB de RAM e SSD de 1TB",
+      Preço: 1799.99,
+      Estoque: 15
+    },
+  ];
+  
+  console.log(produtosEletronicos);
+  
